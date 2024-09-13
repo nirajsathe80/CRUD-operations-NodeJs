a68a914b48dc2285d950bc99d9f835689c2ea1c9
nirajsathe80 <nirajsathe80@gmail.com>
2024-09-13 18:11:47 +0530

    auth middlewade add

---
+++ middleware/authHandler.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+const jwt = require("jsonwebtoken");
+
+const validateAuthToekn = asyncHandler(async (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401);
+        throw new Error("Authentication Failed");
+      }
+      if (!decoded) {
+        res.status(401);
+        throw new Error("Authentication Failed");
+      }
+
+      req.user = decoded.user;
+      next();
+    });
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Token Required");
+  }
+});
+
+module.exports = validateAuthToekn;
